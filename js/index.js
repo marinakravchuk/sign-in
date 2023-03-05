b1df37469e7bec775e25ac3fcccbaa2cb6bdbfc6
marinakravchuk <105381706+marinakravchuk@users.noreply.github.com>
2023-03-05 16:48:01 +0200

    js

---
+++ js/index.js
@@ -0,0 +1,13 @@
+const btnSignIn = document.querySelector(".form_btn")
+
+
+btnSignIn.addEventListener("click", () => login() )
+
+function login() {
+    if ($("#username").val() == "admin" && $("#password").val() == "password") {
+      alert("You are a valid user");
+    } else {
+      alert("You are not a valid user");
+    }
+  }
+  
